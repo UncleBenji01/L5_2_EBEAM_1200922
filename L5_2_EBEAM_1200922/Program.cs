afd79c9593358622881274898a48f6a5fcd0f457
Estudiante <Estudiante@T203-03-2022>
2022-09-26 21:54:02 +0200

    Agregar archivos de proyecto.

---
+++ L5_2_EBEAM_1200922/Program.cs
@@ -0,0 +1,42 @@
+﻿int ndia;
+Console.WriteLine("Ejercicio 2");
+Console.WriteLine("Ingrese el numero de dia");
+ndia = Convert.ToInt32(Console.ReadLine());
+if (ndia < 0)
+{
+    Console.WriteLine("Error: El numero a ingresar debe estar contenido 1 y 7");
+}
+else if (ndia > 7)
+{
+    Console.WriteLine(" Error: El numero a ingresar debe estar contenido 1 y 7");
+
+}
+
+ if (ndia == 1)
+{
+    Console.WriteLine(" Lunes ");
+}
+ if (ndia == 2)
+{
+    Console.WriteLine(" Martes ");
+}
+ if (ndia == 3)
+{
+    Console.WriteLine(" Miercoles ");
+}
+if (ndia == 4)
+{
+    Console.WriteLine(" Jueves ");
+}
+ if (ndia == 5)
+{
+    Console.WriteLine(" Viernes ");
+}
+if (ndia == 6)
+{
+    Console.WriteLine(" Sabado ");
+}
+if (ndia == 7)
+{
+    Console.WriteLine(" Domingo ");
+}
